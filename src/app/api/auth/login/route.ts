3b84e3ce068191cf228922b3a444367730cbd424
Mohak Bajaj <bmohak87@gmail.com>
2024-02-27 10:47:22 +0530

    Add login route and remove console.log

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,28 +1,26 @@
-import { validateEmail, validatePassword, validateUsername } from "@/lib/utils";
+import { validateEmail, validatePassword } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const { email, username, password } = body;
+  const { email, password } = body;
 
   if (
     !email ||
-    !username ||
     !password ||
     !validateEmail(email) ||
-    !validateUsername(username) ||
     !validatePassword(password)
   ) {
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   }
 
-  const response = await fetch("http://localhost:5000/auth/register", {
+  const response = await fetch("http://localhost:5000/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-server-identity": process.env.SERVER_CONNECTION_SECRET || "",
     },
-    body: JSON.stringify({ email, username, password }),
+    body: JSON.stringify({ email, password }),
   });
 
   const data = await response.json();
