36ab4d2b3140d91df8ba396b4c67e83b435abeba
Mohak Bajaj <bmohak87@gmail.com>
2024-03-09 20:24:42 +0530

    Add email verification routes and utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import z from "zod";
+import { createHash } from "crypto";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -29,4 +30,25 @@ export function validateUsername(username: string): boolean {
     .max(50)
     .regex(/^[a-zA-Z0-9._]+$/);
   return usernameSchema.safeParse(username).success;
+}
+
+export function generateCode(email: string): string {
+  const randomCode = Math.floor(Math.random() * 1000)
+    .toString()
+    .padStart(3, "0");
+  const hash = createHash("sha256");
+  hash.update(email + randomCode);
+  for (let i = 0; i < 10; i++) {
+    hash.update(email);
+  }
+  return hash.digest("hex").slice(0, 9) + randomCode;
+}
+
+export function verifyCode(email: string, code: string): boolean {
+  const hash = createHash("sha256");
+  hash.update(email + code.slice(9));
+  for (let i = 0; i < 10; i++) {
+    hash.update(email);
+  }
+  return hash.digest("hex").slice(0, 9) === code.slice(0, 9);
 }
