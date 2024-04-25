cdb11a41d5c82611116da7e7f3288e3dbd90e5c6
Mohak Bajaj <bmohak87@gmail.com>
2024-04-25 23:27:41 +0530

    Update dashboard page with new components

--- src/components/dashboard/feed.tsx
+++ src/components/dashboard/feed.tsx
@@ -3,15 +3,56 @@ import Post from "./post";
 import UserPost from "./user-post";
 
 export default function Feed() {
+  const dummy_data = [
+    {
+      user: "John Doe",
+      post: "Hello, World!",
+      likes: 0,
+      dislikes: 0,
+      tags: ["general", "academic"],
+    },
+    {
+      user: "John Doe",
+      post: "Hello, World!",
+      likes: 0,
+      dislikes: 0,
+      tags: ["general", "academic"],
+    },
+    {
+      user: "John Doe",
+      post: "Hello, World!",
+      likes: 0,
+      dislikes: 0,
+      tags: ["general", "academic"],
+    },
+    {
+      user: "John Doe",
+      post: "Hello, World!",
+      likes: 0,
+      dislikes: 0,
+      tags: ["general", "academic"],
+    },
+    {
+      user: "John Doe",
+      post: "Hello, World!",
+      likes: 0,
+      dislikes: 0,
+      tags: ["general", "academic"],
+    },
+  ];
   return (
     <div className="col-span-2 border-r overflow-y-auto">
       <Post />
-      <UserPost />
-      <UserPost />
-      <UserPost />
-      <UserPost />
-      <UserPost />
-      <UserPost />
+      {dummy_data.map((data, index) => (
+        <UserPost
+          key={index}
+          user={data.user}
+          post={data.post}
+          likes={data.likes}
+          dislikes={data.dislikes}
+          tags={data.tags}
+        />
+      ))}
     </div>
   );
 }
--- src/components/dashboard/user-post.tsx
+++ src/components/dashboard/user-post.tsx
@@ -2,30 +2,49 @@ import { ThumbsDown, ThumbsUp } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 
-export default function UserPost() {
+export default function UserPost({
+  user,
+  post,
+  likes,
+  dislikes,
+  tags,
+}: {
+  user: string;
+  post: string;
+  likes: number;
+  dislikes: number;
+  tags: string[];
+}) {
   return (
     <div className="flex flex-wrap gap-2 border-b p-2">
       <div className="w-full flex gap-2 items-center">
         <Avatar className="pointer-events-none select-none">
-          <AvatarImage src="https://source.boringavatars.com/beam/120/JohnDoe" />
-          <AvatarFallback>JohnDoe</AvatarFallback>
+          <AvatarImage
+            src={`https://source.boringavatars.com/beam/120/${user}`}
+          />
+          <AvatarFallback>{user}</AvatarFallback>
         </Avatar>
-        <p className="font-semibold text-sm">john_doe</p>
+        <p className="font-semibold text-sm">{user}</p>
       </div>
-      <p className="w-full text-muted-foreground">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptates, dolorum, quos, quas voluptatem quod quibusdam atque
-        necessitatibus quia maiores quidem. Quisquam voluptates, dolorum, quos,
-        quas voluptatem quod quibusdam atque necessitatibus quia maiores quidem.
-      </p>
-      <div className="flex gap-2">
+      <p className="w-full text-muted-foreground">{post}</p>
+      <div className="flex w-full items-center gap-1 select-none">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="px-2 py-1 bg-primary text-foreground h-5 rounded-full flex items-center text-sm"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+      <div className="flex gap-2 select-none">
         <Button variant={"ghost"}>
-          <ThumbsUp />
-          &nbsp; 100
+          <ThumbsUp className="w-5 h-5" />
+          &nbsp; {likes}
         </Button>
         <Button variant={"ghost"}>
-          <ThumbsDown />
-          &nbsp; 23
+          <ThumbsDown className="w-5 h-5" />
+          &nbsp; {dislikes}
         </Button>
       </div>
     </div>
